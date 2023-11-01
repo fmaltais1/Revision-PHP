dff3ef1adc57ee60ee4133fc0b60d3ee70971818
fmaltais <frankmaltaiss@gmail.com>
2023-10-31 20:37:04 -0400

    Ajout de la revision 1 sur Ajax

---
+++ Revision AJAX/js/javascript.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", () => {
+    updateLights();
+})
+
+
+const updateLights = () => {
+    console.log("test");
+    fetch("ajax.php")
+    .then(response => response.json())
+    .then(response => {
+        response.forEach(light => {
+            document.querySelector('#light-' +light.id).src = "img/light-" + (light.status == "1" ? "on" : "off") + ".png";
+        })
+    })
+
+    
+    setTimeout(updateLights, 5000);
+
+}
